fbe6fdd91d14e0560a7ec47d1e74c1ef780387fa
hanson.h <hanson.h@classting.com>
2021-12-09 19:37:54 +0900

    rusoto 대신 aws-sdk-s3 를 사용함

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,33 @@
-use rusoto_core::credential::{AwsCredentials, DefaultCredentialsProvider, StaticProvider};
-use rusoto_core::Region;
-use rusoto_s3::{ListObjectsRequest, S3Client, S3};
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_s3::{Client, Error, Region};
 
 #[tokio::main]
 async fn main() {
-    let list_request = ListObjectsRequest {
-        delimiter: Some("/".to_owned()),
-        bucket: "classting-archive".to_owned(),
-        ..Default::default()
-    };
+    // TODO: args 가 1 보다 작을 시 usage 메시지를 남길 것
+    let bucket = std::env::args().collect::<Vec<String>>()[1].clone();
 
-    let client = S3Client::new_with(
-        rusoto_core::request::HttpClient::new().unwrap(),
-        DefaultCredentialsProvider::new().unwrap(),
-        Region::ApNortheast1,
-    );
+    // TODO: args 로 region을 선택받을 수 있게 할 것
+    let region_provider =
+        RegionProviderChain::default_provider().or_else(Region::new("ap-northeast-1"));
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+
+    let client = Client::new(&shared_config);
 
     let resp = client
-        .list_objects(list_request)
+        .list_objects_v2()
+        .bucket(&bucket)
+        .delimiter("/")
+        .send()
         .await
-        .expect("list objects failed");
+        .unwrap();
 
-    let objs = resp.contents.unwrap();
-    objs.iter()
-        .for_each(|m| println!("{}", m.key.as_ref().unwrap()));
-    resp.common_prefixes
-        .unwrap()
-        .iter()
-        .for_each(|m| println!("{}", m.prefix.as_ref().unwrap()));
+    println!("Objects:");
+
+    for object in resp.common_prefixes().unwrap_or_default() {
+        println!("{}", object.prefix().unwrap_or_default());
+    }
+
+    for object in resp.contents().unwrap_or_default() {
+        println!("{}", object.key().unwrap_or_default());
+    }
 }
