ac7358ec3dcadc92f17da0cc8226f37d3964e910
hanson.h <hanson.h@classting.com>
2021-12-17 18:50:51 +0900

    Get all tables

---
+++ src/glue/client.rs
@@ -0,0 +1,43 @@
+use std::rc::Rc;
+
+use aws_sdk_glue::model::{Database, Table};
+use eyre::Result;
+
+#[derive(Debug)]
+pub struct GlueTable(Rc<Database>, Table);
+
+pub async fn get_all_glue_tables() -> Result<Vec<GlueTable>> {
+    let shared_config = aws_config::load_from_env().await;
+    let client = aws_sdk_glue::Client::new(&shared_config);
+
+    if let Some(database_list) = client.get_databases().send().await?.database_list() {
+        let database_table_list = futures::future::join_all(database_list.into_iter().map(|d| {
+            let tables = client
+                .get_tables()
+                .set_database_name(d.name().map(|n| n.to_owned()))
+                .send();
+            let database = futures_util::future::ready(d.to_owned());
+            futures_util::future::join(database, tables)
+        }))
+        .await;
+
+        Ok(database_table_list
+            .into_iter()
+            .filter_map(|(database, table_output)| table_output.ok().map(|t| (database, t)))
+            .filter_map(|(database, table_output)| {
+                table_output
+                    .table_list()
+                    .map(|table_list| (database, table_list.to_owned()))
+            })
+            .map(|(database, table_list)| {
+                let database = Rc::new(database);
+                table_list
+                    .into_iter()
+                    .map(move |t| GlueTable(database.clone(), t))
+            })
+            .flatten()
+            .collect())
+    } else {
+        Err(eyre::eyre!("No database in Glue"))
+    }
+}
--- src/glue/main.rs
+++ src/glue/main.rs
@@ -1,6 +1,10 @@
 use eyre::Result;
 
+use tui_aws::glue::client::get_all_glue_tables;
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    println!("{:?}", get_all_glue_tables().await);
+
     Ok(())
 }
